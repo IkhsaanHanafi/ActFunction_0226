f82bb490e34d537c19305f14f7ed7ffaa362a627
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2026-03-02 22:13:35 +0700

    implementasi fungsi dan prosedur parameter

--- fungsidenganparameter.cpp
+++ fungsidenganparameter.cpp
@@ -5,3 +5,11 @@ using namespace std;
 // deklarasi variabel global
 int panjang, lebar;
 
+// implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+}
+
